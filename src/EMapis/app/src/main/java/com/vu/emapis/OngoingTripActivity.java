8834a6d1dff36cc9782d5c51bf2543a1f5906e98
debe7408 <deividas.bendaravicius@mif.stud.vu.lt>
2021-10-21 00:42:55 +0300

    Added trip chronometer and fixed the layound

--- src/EMapis/app/src/main/java/com/vu/emapis/OngoingTripActivity.java
+++ src/EMapis/app/src/main/java/com/vu/emapis/OngoingTripActivity.java
@@ -2,21 +2,47 @@ package com.vu.emapis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
 public class OngoingTripActivity extends AppCompatActivity {
+
+    Chronometer simpleChronometer;
+    boolean clicked = false;
+    private long lastPause;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ongoing_trip);
 
+        simpleChronometer = findViewById(R.id.simpleChronometer);
+        simpleChronometer.start();
+        simpleChronometer.setBase(SystemClock.elapsedRealtime());
 
     }
 
+    public void pauseTripOnClick(View view) {
 
+        TextView textView = findViewById(R.id.textView2);
+        Button button = findViewById(R.id.button2);
 
+        if(!clicked) {
+            lastPause = SystemClock.elapsedRealtime();
+            simpleChronometer.stop();
+            textView.setText("Trip is paused");
+            button.setText("Resume the trip");
+            clicked = true;
+        } else {
+            simpleChronometer.setBase(simpleChronometer.getBase() + SystemClock.elapsedRealtime() - lastPause);
+            simpleChronometer.start();
+            textView.setText("Trip is in progress");
+            button.setText("Pause the trip");
+            clicked = false;
+        }
+    }
 }
