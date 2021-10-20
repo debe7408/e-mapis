9c57870f94b9e1523a7e296146187699b8177fe7
Valma Julius <julius.valma@mif.stud.vu.lt>
2021-10-20 21:13:57 +0300

    Added ongoing trip screen

--- src/EMapis/app/src/main/java/com/vu/emapis/OngoingTripActivity.java
+++ src/EMapis/app/src/main/java/com/vu/emapis/OngoingTripActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class OngoingTrip extends AppCompatActivity {
+public class OngoingTripActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
