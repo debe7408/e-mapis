1a17dc6e4c00b00edad9e92fe0fde27be05a282f
emkah4 <sherys.emilis@gmail.com>
2021-11-02 21:30:06 +0200

    sss

--- src/EMapis/app/src/main/java/com/vu/emapis/TripEndActivity.java
+++ src/EMapis/app/src/main/java/com/vu/emapis/TripEndActivity.java
@@ -1,17 +1,25 @@
 package com.vu.emapis;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class TripEndActivity extends AppCompatActivity {
 
     private SeekBar seekBar;
     private TextView textView;
-    private Button button;
+
+    @BindView(R.id.btn_showStats)
+    Button btnShowStats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +28,13 @@ public class TripEndActivity extends AppCompatActivity {
 
         seekBar = findViewById(R.id.rechargedEnergyLevels);
         textView = findViewById(R.id.energyLevelText);
-        button = findViewById(R.id.btn_showStats);
 
         seekBarInit();
     }
 
 
     public void seekBarInit() {
-        textView.setText("Energy levels: "+seekBar.getProgress() + "%");
+        textView.setText("Energy levels: " + seekBar.getProgress() + "%");
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
@@ -48,6 +55,11 @@ public class TripEndActivity extends AppCompatActivity {
                 textView.setText("Energy levels: "+progressValue + "%");
             }
         });
+    }
+
+    public void showStatistics(View view) {
+        Intent intent = new Intent(TripEndActivity.this, VolleyActivity.class);
+        startActivity(intent);
     }
 
 }
