083b9135af5d1ce38b3600efc42959524f8aaaf1
emkah4 <sherys.emilis@gmail.com>
2021-11-08 00:30:25 +0200

    pw hashing added

--- src/EMapis/app/src/main/java/com/vu/emapis/RegisterActivity.java
+++ src/EMapis/app/src/main/java/com/vu/emapis/RegisterActivity.java
@@ -4,13 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 public class RegisterActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.vu.emapis.MESSAGE";
+
+    private String url ="http://193.219.91.103:8666/users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +44,65 @@ public class RegisterActivity extends AppCompatActivity {
         EditText txtPassword = findViewById(R.id.registerPasswordText);
         EditText txtEmail = findViewById(R.id.registerEmailText);
 
-        Intent intent = new Intent(this, MainScreenActivity.class);
         String username = txtUserName.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE,username);
-        startActivity(intent);
+        String password = txtPassword.getText().toString();
+        String email = txtEmail.getText().toString();
+
+        if(username.matches("") || password.matches("") || email.matches("")) {
+            Toast.makeText(this, "All fields must be filled", Toast.LENGTH_SHORT).show();
+        } else {
+
+            String hashPassword = "12345";
+            String bcryptHashString = BCrypt.withDefaults().hashToString(12, hashPassword.toCharArray());
+            Log.d("register", bcryptHashString);
+            BCrypt.Result result = BCrypt.verifyer().verify(hashPassword.toCharArray(), bcryptHashString);
+            if (result.verified) {
+                Toast.makeText(this, "Password hashed", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Password not hashed", Toast.LENGTH_SHORT).show();
+            }
+
+            sendPostRequest(username, bcryptHashString, email);
+
+            Intent intent = new Intent(this, MainScreenActivity.class);
+            intent.putExtra(EXTRA_MESSAGE,username);
+            startActivity(intent);
+        }
+    }
+
+
+    private void sendPostRequest(String username, String password, String email) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("email", email);
+            postData.put("username", username);
+            postData.put("password", password);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                System.out.println(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoidG9kb191c2VyIn0.kTNyXxM8oq1xhVwNznb08dlSxIjq1F023zeTWyKNcNY");
+                return headers;
+            }
+        };
+
+        queue.add(jsonObjectRequest);
     }
 }
