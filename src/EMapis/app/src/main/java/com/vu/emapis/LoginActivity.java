2c5e0985f550dfb14b4795a4b80aa4f713ec9f95
emkah4 <sherys.emilis@gmail.com>
2021-11-09 22:53:49 +0200

    post to get password and hahaha extremely potent humorist

--- src/EMapis/app/src/main/java/com/vu/emapis/LoginActivity.java
+++ src/EMapis/app/src/main/java/com/vu/emapis/LoginActivity.java
@@ -6,12 +6,29 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.vu.emapis.MESSAGE";
+    private String url ="http://193.219.91.103:8666/rpc/get_pw";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +39,111 @@ public class LoginActivity extends AppCompatActivity {
     /** This method is called when the user clicks "LOGIN" button **/
     public void onClick(View view) {
 
+        EditText txtUserName = findViewById(R.id.usernameTextField);
+        EditText txtPassword = findViewById(R.id.passwordTextField);
+
+        String username = txtUserName.getText().toString();
+        String password = txtPassword.getText().toString();
+
         Intent intent = new Intent(LoginActivity.this, MainScreenActivity.class); // Start new activity
         EditText editUsername = (EditText) findViewById(R.id.usernameTextField); // find text from username field
-        String username = editUsername.getText().toString(); // EditText -> String transformation
         intent.putExtra(EXTRA_MESSAGE, username); // Adds extra data to intent. (nameOfData, data)
         startActivity(intent); // Starts the new activity
+
+        sendPostRequest(username);
+    }
+
+    private void sendPostRequest(String username) {
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("x", "cr");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("HERERER", response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoidG9kb191c2VyIn0.kTNyXxM8oq1xhVwNznb08dlSxIjq1F023zeTWyKNcNY");
+                return headers;
+            }
+        };
+
+        requestQueue.add(jsonObjectRequest);
+
+        /*try {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("x", username);
+            final String requestBody = jsonBody.toString();
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("xddd", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("VOLLEY", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> headers = new HashMap<>();
+                    headers.put("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoidG9kb191c2VyIn0.kTNyXxM8oq1xhVwNznb08dlSxIjq1F023zeTWyKNcNY");
+                    return headers;
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        */
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+
+        /*JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("xddd", response.toString());
+                System.out.println(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoidG9kb191c2VyIn0.kTNyXxM8oq1xhVwNznb08dlSxIjq1F023zeTWyKNcNY");
+                return headers;
+            }
+        };
+
+        queue.add(jsonObjectRequest);*/
     }
 
     public void onClickRegister(View view) {
