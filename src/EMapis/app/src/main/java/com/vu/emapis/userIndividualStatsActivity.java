4f60b4bf8c4b6c10dbf90372cde88b14595ab0e0
debe7408 <deividas.bendaravicius@mif.stud.vu.lt>
2022-01-07 02:14:30 +0200

    Managed to retrieve all trips by user from database and placed them in a ListView

--- src/EMapis/app/src/main/java/com/vu/emapis/userIndividualStatsActivity.java
+++ src/EMapis/app/src/main/java/com/vu/emapis/userIndividualStatsActivity.java
@@ -2,8 +2,12 @@ package com.vu.emapis;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,10 +19,12 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
+import com.google.gson.internal.bind.ObjectTypeAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,6 +34,8 @@ public class userIndividualStatsActivity extends AppCompatActivity {
     private ListView listView;
 
     private statisticsObject[] stats;
+    private ArrayList<String> statsArray = new ArrayList<String>();
+
 
     public interface VolleyCallbackGet {
         void onSuccess(JSONArray result) throws JSONException;
@@ -40,37 +48,57 @@ public class userIndividualStatsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_individual_stats);
 
+        // Declare widgets
         listView = findViewById(R.id.ListView);
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
-
-        listView.setAdapter(arrayAdapter);
-
-
+        // URL for the get request
         String url = "http://193.219.91.103:4558/trips?user_id=eq."+ LoginActivity.userId;
+
+
+        // Call a new get request
         sendGetRequest(url, new VolleyCallbackGet() {
+
+            // If the get request is successful execute this
             @Override
             public void onSuccess(JSONArray result) throws JSONException {
 
-
-                if(stats == null) {
+                // If the user has no trips recorded, the activity will exit
+                if(stats == null || stats.length <= 0) {
                     Log.d("Success", "Empty");
-
+                    Toast.makeText(userIndividualStatsActivity.this, "Seems like you don't have any trips yet", Toast.LENGTH_SHORT).show();
+                    finish();
                 }
 
-                for(int i=0; i<stats.length; i++) {
-                    Log.d("Success",stats[i].toString());
+                // Otherwise, store data to an ArrayList and display it in the ListView
+                else {
+                    for(int i=0; i<stats.length; i++) {
+                        Log.d("Success", "trip id = "+ stats[i].getTrip_id());
+
+                        statsArray.add("Trip ID = " + stats[i].getTrip_id());
+                    }
                 }
-
-
+                ArrayAdapter arrayAdapter = new ArrayAdapter(userIndividualStatsActivity.this, android.R.layout.simple_list_item_1, statsArray);
+                listView.setAdapter(arrayAdapter);
             }
 
+            // When error accures, do something
             @Override
             public void onError(String error) {
                 Log.d("error", error);
+                //TODO Implement on error handling
             }
         });
 
+        // Click listener for the ListView
+       listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+               Object o = listView.getItemAtPosition(position);
+               Toast.makeText(userIndividualStatsActivity.this, "Hello " + o, Toast.LENGTH_SHORT).show();
+
+           }
+       });
 
     }
 
@@ -83,6 +111,7 @@ public class userIndividualStatsActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONArray response) {
 
+                        // JSON -> GSON
                         Gson gson = new Gson();
                         stats = gson.fromJson(String.valueOf(response), statisticsObject[].class);
 
